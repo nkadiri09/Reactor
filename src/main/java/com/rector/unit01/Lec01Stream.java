f1b1f558e0836ef20275a53a9667b7490c21e8c9
Narendra <nkadiri09@gmail.com>
2022-06-26 23:33:44 -0400

    Lazy stream

--- src/main/java/com/rector/unit01/Lec01Stream.java
+++ src/main/java/com/rector/unit01/Lec01Stream.java
@@ -1,10 +1,12 @@
 package com.rector.unit01;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Lec01Stream {
     public static void main(String[] args) {
-        Stream<Integer> integerStream = Stream.of(1)
+        Stream<Integer> integerStream = Stream.of(1,2,3,4,5)
                 .map(i -> {
                     System.out.println("Test");
                     try {
@@ -15,7 +17,19 @@ public class Lec01Stream {
                     System.out.println("Test11111");
                     return i * 2;
                 });
+        // Stream is lazy initialized
         System.out.println(integerStream);
+
+        //intermediate operations
+        //List<Integer> collect = integerStream.map(a -> a * a).collect(Collectors.toList());
+        // System.out.println("collect is: "+collect);
+
+
+        /**
+         *  Terminal operation
+         *  Stream will execute whenever we connect with Terminal operator.
+         */
+
         integerStream.forEach(System.out::println);
     }
 }

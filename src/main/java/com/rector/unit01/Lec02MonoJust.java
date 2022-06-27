dd5bb7a5d21721fbe3f750f74f7745280ca5ee4b
Narendra <nkadiri09@gmail.com>
2022-06-27 00:54:34 -0400

    Added Mono just and refactored

--- src/main/java/com/rector/unit01/Lec02MonoJust.java
+++ src/main/java/com/rector/unit01/Lec02MonoJust.java
@@ -7,9 +7,11 @@ public class Lec02MonoJust {
     public static void main(String[] args) {
         Mono<Integer> mono = Mono.just(10);
         // Mono also lazily initialized
-        System.out.println("mono is: "+mono);
+        System.out.println("mono is: " + mono);
 
         // we can log information using mono.log
         mono.log().subscribe(System.out::println);
+
+        mono.subscribe(i -> System.out.println("Received :" + i));
     }
 }

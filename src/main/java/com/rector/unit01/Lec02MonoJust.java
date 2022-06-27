018438fc57ed8852894da88f55e1594efa632a72
Narendra <nkadiri09@gmail.com>
2022-06-27 00:48:53 -0400

    Added Mono just

---
+++ src/main/java/com/rector/unit01/Lec02MonoJust.java
@@ -0,0 +1,15 @@
+package com.rector.unit01;
+
+import reactor.core.publisher.Mono;
+
+public class Lec02MonoJust {
+
+    public static void main(String[] args) {
+        Mono<Integer> mono = Mono.just(10);
+        // Mono also lazily initialized
+        System.out.println("mono is: "+mono);
+
+        // we can log information using mono.log
+        mono.log().subscribe(System.out::println);
+    }
+}
